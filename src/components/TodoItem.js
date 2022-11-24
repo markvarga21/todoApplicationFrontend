96564ef2f62478c5dfbb2caa4800210efe776e2c
Varga József Márk <vmark2145@gmail.com>
2022-11-24 13:32:22 +0100

    Further design changes

---
+++ src/components/TodoItem.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  Img,
+  Flex,
+  Center,
+  useColorModeValue,
+  HStack,
+  Image,
+  Divider,
+} from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const TodoItem = ({ todo }) => {
+  return (
+    <Box
+      w="xs"
+      rounded={"sm"}
+      my={5}
+      mx={[0, 5]}
+      overflow={"hidden"}
+      bg="white"
+      border={"1px"}
+      borderColor="black"
+      boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 cyan")}
+    >
+      <Box h={"100px"} borderBottom={"1px"} borderColor="black">
+        <Image
+          src={
+            "https://media.istockphoto.com/id/1173780504/vector/to-do-list.jpg?s=612x612&w=0&k=20&c=v9CFBAPGBYqEZnu8Hn1ZNIuIHlk-RBj-OtMpB8WfbP4="
+          }
+          roundedTop={"sm"}
+          objectFit="cover"
+          h="full"
+          w="full"
+          alt={"Todo Image"}
+        />
+      </Box>
+      <Box p={4}>
+        <Heading color={"black"} fontSize={"2xl"} noOfLines={1}>
+          {todo.title}
+        </Heading>
+        <Text color={"black.500"} fontSize={"xl"}>
+          {todo.description}
+        </Text>
+        <Box>
+          <FontAwesomeIcon icon="fa-solid fa-location-pin" />
+          <Text color={"gray.500"}>{todo.location}</Text>
+        </Box>
+        <Text color={"gray.500"}>{todo.date}</Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import TodoItem from "./components/TodoItem";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
     <React.StrictMode>
-      <App />
+      {/* <App /> */}
+      <TodoItem
+        todo={{
+          title: "Dishes",
+          description: "I have to walk the dog ASAP",
+          location: "Debrecen Csapo utca 88",
+          date: "2022.12.10 10:20",
+        }}
+      />
     </React.StrictMode>
   </ChakraProvider>
 );
