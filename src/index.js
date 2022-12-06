ae7291c8893867f3578760d360f859a7752053a9
Varga József Márk <vmark2145@gmail.com>
2022-12-06 19:26:56 +0100

    Started user related components

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
+import Register from "./components/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -19,6 +20,7 @@ root.render(
           <Route path="/" element={<App />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
